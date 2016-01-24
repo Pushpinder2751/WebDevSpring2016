1e2eede02abecfff68c896f0fad4714428161270
pushpinder2751 <pushpinder2751@yahoo.com>
2016-01-24 17:03:27 -0500

    added /api/users

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+
+// where to fetch the static content
+// __dirname = directory name
+// '/public' from public direcory
 app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
+
+app.get('/api/users',function(req,res){
+    var users = [
+        {username: 'Pushpinder', first: 'Pushpinder', last: 'Singh'},
+        {username: 'Mavez', first: 'Mavez', last: 'Singh'},
+        {username: 'Rahul', first: 'Rahul', last: 'Bahal'},
+        {username: 'Parth', first: 'Parth', last: 'Aluwalia'},
+    ];
+    res.json(users);
+    //can also write res.send(users);
+    // send automatically detects json
+});
+
+
+
 app.listen(port, ipaddress);
