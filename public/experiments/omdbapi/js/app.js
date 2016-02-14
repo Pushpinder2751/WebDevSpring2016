d6309627cd776d2713ed88f5d38131e0f81a84e9
pushpinder2751 <pushpinder2751@yahoo.com>
2016-02-14 15:50:26 -0500

    expi

---
+++ public/experiments/omdbapi/js/app.js
@@ -0,0 +1,8 @@
+//creating an IIFE or Immediately-Invoked Function Expressions to protect the namespace
+(function(){
+    $(init);
+
+    function init(){
+        alert("Hello form JQuery");
+    } 
+})();
