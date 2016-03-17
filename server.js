f1eb69b04f7de8abd01d64d08ba5a8cfc400072a
pushpinder2751 <pushpinder2751@yahoo.com>
2016-03-16 20:30:13 -0400

    Assignment-3 implentation

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
 // where to fetch the static content
 // __dirname = directory name
 // '/public' from public direcory
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
